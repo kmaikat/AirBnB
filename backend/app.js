6ac99c2feacff9821cd0ea724c959a04387b3579
Katherine <katherinepmai@gmail.com>
2022-10-30 21:13:32 -0500

    removed stack in production under app.js

--- backend/app.js
+++ backend/app.js
@@ -18,27 +18,27 @@ app.use(express.json());
 
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
-  }
+  // enable cors only in development
+  app.use(cors());
+}
 
-  // helmet helps set a variety of headers to better secure your app
-  app.use(
-    helmet.crossOriginResourcePolicy({
-      policy: "cross-origin"
-    })
-  );
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
 
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 
 const routes = require('./routes');
 
@@ -52,12 +52,12 @@ const { ValidationError } = require('sequelize');
 
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
-    const err = new Error("The requested resource couldn't be found.");
-    err.title = "Resource Not Found";
-    err.errors = ["The requested resource couldn't be found."];
-    err.status = 404;
-    next(err);
-  });
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
 
 // Process sequelize errors
 app.use((err, _req, _res, next) => {
@@ -71,16 +71,18 @@ app.use((err, _req, _res, next) => {
 
 // Error formatter
 app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    console.error(err);
-    res.json({
-      // title: err.title || 'Server Error',
-      message: err.message,
-      statusCode: err.status,
-      errors: err.errors,
-      stack: isProduction ? null : err.stack
-    });
-  });
+  res.status(err.status || 500);
+  console.error(err);
+  const response = {
+    message: err.message,
+    statusCode: err.status,
+    errors: err.errors,
+  }
+
+  if (isProduction) err.stack = err.stack
+  
+  res.json(response);
+});
 
 
 
