8f33dcb4c3d60af76765e81a24c2cc1b93fba81e
Katherine <katherinepmai@gmail.com>
2022-11-13 20:53:50 -0600

    frontend setup redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,14 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
