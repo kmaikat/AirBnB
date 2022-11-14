83b665a4d52662a33bcb3fdb1e9493aac7a6b3a4
Katherine <katherinepmai@gmail.com>
2022-11-14 01:15:00 -0600

    created signup action thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,11 +33,29 @@ export const login = (user) => async (dispatch) => {
 };
 
 export const restoreUser = () => async dispatch => {
-    const response = await csrfFetch('/api/session');
-    const data = await response.json();
-    dispatch(setUser(data.user));
-    return response;
-  };
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password, firstName, lastName } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+      firstName,
+      lastName
+    }),
+  });
+
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+}
 
 const initialState = { user: null };
 
