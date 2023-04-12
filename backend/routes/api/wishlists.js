c2e724d4a23f680744828ca80065391c017d50dd
Katherine <katherinepmai@gmail.com>
2023-04-11 19:55:43 -0500

    Create middleware to validate name of the wishlist

--- backend/routes/api/wishlists.js
+++ backend/routes/api/wishlists.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const { Wishlist, WishlistItem, Spot, User } = require("../../db/models");
+const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
 
+const validateWishlistName = [
+    check("name")
+        .exists({ checkFalsy: true })
+        .withMessage("Please provide your wishlist with a name")
+        .isLength({ max: 50 })
+        .withMessage("Name must be less than 50 characters")
+]
+// Get all wishlists
 router.get('/', async (req, res, next) => {
     const userId = parseInt(req.user.id)
     const wishlists = await Wishlist.findAll({
@@ -16,5 +25,19 @@ router.get('/', async (req, res, next) => {
 
     return res.json({Wishlists: wishlists})
 })
+
+// create a wishlist
+router.post('/', validateWishlistName, async (req, res, next) => {
+
+})
+
+// edit a wishlist (name)
+
+// delete a wishlist
+
+// add spot to wishlist
+
+// delete a spot in wishlist
+
 
 module.exports = router;
