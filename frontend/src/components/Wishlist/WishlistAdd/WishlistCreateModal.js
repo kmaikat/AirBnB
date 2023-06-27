5d80e1def2d8a953c6b6f4660a714707371db757
Katherine <katherinepmai@gmail.com>
2023-06-27 12:20:55 -0500

    added a character limit to wishlist name

--- frontend/src/components/Wishlist/WishlistAdd/WishlistCreateModal.js
+++ frontend/src/components/Wishlist/WishlistAdd/WishlistCreateModal.js
@@ -11,7 +11,7 @@ const WishlistCreateModal = ({ setShowModal }) => {
     return (
         <div className="create-modal-outer-container">
             <div className="create-modal-heading-container">
-                <button onClick={event => {event.stopPropagation(); setShowModal(false)}}>
+                <button onClick={event => { event.stopPropagation(); setShowModal(false) }}>
                     <i className="fa-solid fa-x"></i>
                 </button>
                 <div>Create wishlist</div>
@@ -19,7 +19,7 @@ const WishlistCreateModal = ({ setShowModal }) => {
             </div>
             <form>
                 <div id="create-modal-name-container">
-                    <label className={name.length > 0 ? "name-label-filled": ""}>Name</label>
+                    <label className={name.length > 0 ? "name-label-filled" : ""}>Name</label>
                     <input
                         name="name"
                         type="text"
@@ -29,11 +29,11 @@ const WishlistCreateModal = ({ setShowModal }) => {
                     />
                 </div>
                 <div className="create-modal-counter-container">
-                <div className="create-modal-counter">{name.length}/50 characters</div>
-                <div className="create-modal-counter-container-warning">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" aria-label="Error" role="img" focusable="false" style={{display: "block", height: "16px", width: "16px", fill: "currentcolor"}}><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zm0 10.2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm.8-6.6H7.2v5.2h1.6z"></path></svg>
-                    <p>Over character limit</p>
-                </div>
+                    <div className="create-modal-counter">{name.length}/50 characters</div>
+                    <div className={name.length >= 50 ? "create-modal-counter-container-warning" : "create-modal-counter-container-warning-hidden"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" aria-label="Error" role="img" focusable="false" style={{ display: "block", height: "16px", width: "16px", fill: "currentcolor" }}><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zm0 10.2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm.8-6.6H7.2v5.2h1.6z"></path></svg>
+                        <p>Over character limit</p>
+                    </div>
                 </div>
             </form>
             <div>
