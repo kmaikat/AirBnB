7e2fb9cd3b67947c8c6f03e40e0cf448f88fe3d8
Katherine <katherinepmai@gmail.com>
2023-06-27 17:07:20 -0500

    styled wishlist cards in the add to wishlist modal

--- frontend/src/components/Wishlist/WishlistAdd/WishlistAddModal.js
+++ frontend/src/components/Wishlist/WishlistAdd/WishlistAddModal.js
@@ -1,17 +1,18 @@
 import { useSelector } from "react-redux"
-import WishlistCard from "../WishlistCard"
-import WishlistAddCard from "./WishlistAddCards"
+import WishlistAddCard from "./WishlistAddCard"
+import "./WishlistAddModal.css"
 
 function WishlistAddModal({ setShowModal }) {
     const wishlists = useSelector(state => state.wishlists)
 
     return (
-        <div>
+        <div className="add-modal-outer-container">
             <div className="add-modal-heading-container">
                 <button onClick={() => setShowModal(false)}>
                     <i className="fa-solid fa-x"></i>
                 </button>
                 <div>Add to wishlist</div>
+                <div></div>
             </div>
             <div className="add-modal-wishlists-container">
                 {wishlists.map(wishlist => (<WishlistAddCard key={wishlist.id} wishlist={wishlist} />))}
