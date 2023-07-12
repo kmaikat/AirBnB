6cc9b5921afc38d5bfd7f1a8da63720003b4c82b
Katherine <katherinepmai@gmail.com>
2023-07-12 17:09:28 -0500

    range implemented on calendar

--- frontend/src/components/Spots/Bookings/Calendar.js
+++ frontend/src/components/Spots/Bookings/Calendar.js
@@ -1,12 +1,29 @@
-import { format, isSameMonth, isToday } from 'date-fns'
-import isThisYear from 'date-fns/isThisYear'
-import startOfToday from 'date-fns/startOfToday'
-import { DayPicker } from 'react-day-picker'
+import { format } from 'date-fns';
+import { useState } from 'react';
+import { DayPicker, DateRange } from 'react-day-picker'
 import 'react-day-picker/dist/style.css'
 
 const Calendar = () => {
+    const [selectedRange, setSelectedRange] = useState('');
+    const [fromValue, setFromValue] = useState('');
+  const [toValue, setToValue] = useState('');
+
     const today = new Date()
     const defaultMonth = new Date(today.getFullYear(), today.getMonth())
+
+    const handleRangeSelect = (range) => { 
+        setSelectedRange(range);
+        if (range?.from) {
+          setFromValue(format(range.from, 'y-MM-dd'));
+        } else {
+          setFromValue('');
+        }
+        if (range?.to) {
+          setToValue(format(range.to, 'y-MM-dd'));
+        } else {
+          setToValue('');
+        }
+      };
 
     return (
         <DayPicker
@@ -17,6 +34,8 @@ const Calendar = () => {
             numberOfMonths={2}
             pagedNavigation
             mode='range'
+            selected={selectedRange}
+            onSelect={handleRangeSelect}
         />
     )
 }
