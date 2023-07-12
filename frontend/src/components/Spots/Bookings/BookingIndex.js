dd48a126b096563929173251e88719a08c498c57
Katherine <katherinepmai@gmail.com>
2023-07-12 12:20:56 -0500

    bookings calendar outline

--- frontend/src/components/Spots/Bookings/BookingIndex.js
+++ frontend/src/components/Spots/Bookings/BookingIndex.js
@@ -1,10 +1,14 @@
 import { useDispatch } from "react-redux"
 import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min"
 import { deleteASpotThunk } from "../../../store/spotReducer"
+import "./BookingIndex.css"
+import { useState } from "react"
 
-const BookingIndex = ({spot, user, setErrors, spotId}) => {
+const BookingIndex = ({ spot, user, setErrors, spotId }) => {
     const dispatch = useDispatch()
     const history = useHistory()
+
+    const [showCalendar, setShowCalendar] = useState(false)
 
     const handleDelete = async () => {
         const deleteResponse = await dispatch(deleteASpotThunk(spotId))
@@ -16,34 +20,75 @@ const BookingIndex = ({spot, user, setErrors, spotId}) => {
         // useEffect for errors for a form
         // for errors from a button click, create error state and set errors to returned errors
     }
+
+    const handleCalendar = () => {
+        setShowCalendar(true)
+    }
     return (
         <div className="spot-details-right">
-                    <div className="spot-details-right-container">
-                        <div className="spot-details-right-top">
-                            <div className="spot-details-right-price"> <span id="right-spot-price">${spot.price}</span> night</div>
-                            <div className="spot-details-right-reviews">
+            <div className="spot-details-right-container">
+                <div className="spot-details-right-top">
+                    <div className="spot-details-right-price"> <span id="right-spot-price">${spot.price}</span> night</div>
+                    <div className="spot-details-right-reviews">
 
-                                {spot.numReviews > 0 && <>
-                                    <div>
-                                        <i className="fa-solid fa-star"></i>
-                                        {Number(spot.avgStarRating).toFixed(2) || "New"}
-                                    </div>
-                                    <div>
-                                        {spot.numReviews > 0 && <>{spot.numReviews} Reviews</>}
-                                    </div>
-                                </>
-                                }
+                        {spot.numReviews > 0 && <>
+                            <div>
+                                <i className="fa-solid fa-star"></i>
+                                {Number(spot.avgStarRating).toFixed(2) || "New"}
                             </div>
-                        </div>
-                        {user && user.id === spot.Owner.id ? <div className="spot-details-right-bottom">
-
-                            <Link to={`/spot/${spotId}/edit`} className="submit" id="spot-edit"> <button>Edit</button></Link>
-
-                            <button className="submit" id="spot-delete" onClick={handleDelete}>Delete</button>
-
-                        </div> : "hi"}
+                            <div>
+                                {spot.numReviews > 0 && <>{spot.numReviews} Reviews</>}
+                            </div>
+                        </>
+                        }
                     </div>
                 </div>
+                {user && user.id === spot.Owner.id ?
+                    <div className="spot-details-right-bottom">
+
+                        <Link to={`/spot/${spotId}/edit`} className="submit" id="spot-edit"> <button>Edit</button></Link>
+
+                        <button className="submit" id="spot-delete" onClick={handleDelete}>Delete</button>
+
+                    </div> :
+                    <div>
+                        <div className="booking-calendar-container">
+                            <div className="booking-calendar-dates-container" onClick={handleCalendar}>
+                                <div className="booking-calendar-dates-content">
+                                    <div>CHECK-IN</div>
+                                    <div>8/17/2305</div>
+                                </div>
+                                <div className="booking-calendar-dates-content">
+                                    <div>CHECKOUT</div>
+                                    <div>8/19/2350</div>
+                                </div>
+                            </div>
+                                {showCalendar &&
+                                    <div className="calendar-container">This is where the calendar will be</div>}
+                            <div>
+                                <div className="booking-guest-data">
+                                    <div>Guests</div>
+                                    <div>12 guests</div>
+                                </div>
+                                <div className="booking-guest-carat">
+                                    <i className="fa-solid fa-angle-down"></i>
+                                </div>
+
+                            </div>
+                        </div>
+                        {/* <div>
+                            <div>reserve</div>
+                            <div>you wont be charged yet</div>
+                        </div>
+                        <div>
+                            <div>602 a night</div>
+                            <div>cleaning fee</div>
+                            <div>service fee</div>
+                        </div> */}
+                    </div>
+                }
+            </div>
+        </div>
     )
 }
 
